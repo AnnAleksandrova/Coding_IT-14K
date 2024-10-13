9c4c316ce4976fa8328ed8b9fc28416d39435d9d
AnnAleksandrova <ansaleksandrova@outlook.com>
2024-10-13 21:46:10 +0300

    Laba1
    Выполнен вариант 2

---
+++ Laba1_Aleksandrova_IT-14K/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba1 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //вариант 2
+            const double x=1.825, y=18.225, z=-3.298;
+            double g, w;
+            g = Math.Abs(Math.Pow(x,y/x) - Math.Cbrt(y/x));
+            w = (y-x)*((y-z)/(1+Math.Pow(y-x,2)));
+            Console.WriteLine("g={0,9:F3}; w={1,8:F4}", g, w);
+            Console.WriteLine("Нажмите Enter для выхода");
+            Console.ReadLine();
+        }
+    }
+}
+
+
